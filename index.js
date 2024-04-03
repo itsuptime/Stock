dc3e2c87a4295b4ade9af18ef108ce0b0141e60c
itsuptime <161259702+itsuptime@users.noreply.github.com>
2024-04-03 20:59:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.listen(() => console.log("Server started"));
+app.listen(3000,() => console.log("Server started"));
 
 app.get("/", (req, res) => {
   res.send("POWER")
